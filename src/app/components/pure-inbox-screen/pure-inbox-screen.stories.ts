3e010aaa87e7f16a6d8563cba140ccb7200b0dc4
OMSV_PROD\jccornaux <jcconraux@gmail.com>
2022-01-12 16:34:48 +0100

    4- interaction entre screen et composants

--- src/app/components/pure-inbox-screen/pure-inbox-screen.stories.ts
+++ src/app/components/pure-inbox-screen/pure-inbox-screen.stories.ts
@@ -6,6 +6,7 @@ import { PureInboxScreenComponent } from './pure-inbox-screen.component';
 import { NgxsModule, Store } from '@ngxs/store';
 import { TaskModule } from './../task.module';
 import { TasksState } from 'src/app/state/task.state';
+import { fireEvent, within } from '@storybook/testing-library';
 
 export default {
   component: PureInboxScreenComponent,
@@ -28,4 +29,13 @@ export const Default = Template.bind({});
 export const Error = Template.bind({});
 Error.args = {
   error: true,
+};
+
+export const WithInteractions = Template.bind({});
+WithInteractions.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Simulates pinning the first task
+  await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+  // Simulates pinning the third task
+  await fireEvent.click(canvas.getByLabelText('pinTask-3'));
 };
